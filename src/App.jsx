6644be6b317d79bd57adba62f3a9000c20ce005b
iamchristinandy <145363456+iamchristinandy@users.noreply.github.com>
2024-07-24 15:59:22 +0000

    Initialising the project with a few changes

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+const App = () => {
+  return (
+    <div className="currency-converter">
+    <h2 className="converter-title">Currency Converter</h2>
+    <form className="converter-form">
+      <div className="form-group">
+        <label className="form-label">Enter Amount</label>
+        <input type="number" className="form-input" required/>
+      </div>
+      <div className="form-group">
+        <div className="form-section">
+        <label className="form-label">From</label>
+        <div className="currency-select">
+          <img src="https://flagsapi.com/US/flat/64.png" alt="Flag" />
+          <select className="currency-dropdown">
+            <option value="USD" selected>USD</option>
+            <option value="CAD">CAD</option>
+            <option value="INR">INR</option>
+            <option value="NPR">NPR</option>
+          </select>
+        </div>
+        </div>
+      </div>
+    </form>
+    </div>
+  )
+}
+
+export default App
