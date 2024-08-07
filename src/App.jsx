638e5ecca0a53cf573b6e88352755013a365c076
iamchristinandy <145363456+iamchristinandy@users.noreply.github.com>
2024-08-07 17:12:52 +0000

    Addition of new selected flag

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,34 @@ const App = () => {
         <label className="form-label">Enter Amount</label>
         <input type="number" className="form-input" required/>
       </div>
+
       <div className="form-group">
         <div className="form-section">
-        <label className="form-label">From</label>
-        <div className="currency-select">
-          <img src="https://flagsapi.com/US/flat/64.png" alt="Flag" />
-          <select className="currency-dropdown">
-            <option value="USD" selected>USD</option>
-            <option value="CAD">CAD</option>
-            <option value="INR">INR</option>
-            <option value="NPR">NPR</option>
-          </select>
+            <label className="form-label">From</label>
+            <div className="currency-select">
+              <img src="https://flagsapi.com/US/flat/64.png" alt="Flag" />
+              <select className="currency-dropdown">
+                <option value="USD" selected>USD</option>
+                <option value="CAD">CAD</option>
+                <option value="INR">INR</option>
+                <option value="NPR">NPR</option>
+              </select>
+            </div>
         </div>
+
+        <div className="form-section">
+            <label className="form-label">From</label>
+            <div className="currency-select">
+              <img src="https://flagsapi.com/IN/flat/64.png" alt="Flag" />
+              <select className="currency-dropdown">
+                <option value="USD">USD</option>
+                <option value="CAD">CAD</option>
+                <option value="INR" selected>INR</option>
+                <option value="NPR">NPR</option>
+              </select>
+            </div>
         </div>
+
       </div>
     </form>
     </div>
