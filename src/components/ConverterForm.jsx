237d7e0194058b3f299675a1bc177e4db1e8a6aa
iamchristinandy <145363456+iamchristinandy@users.noreply.github.com>
2024-08-15 15:50:44 +0000

    Bug Fixes

--- src/components/ConverterForm.jsx
+++ src/components/ConverterForm.jsx
@@ -1,3 +1,5 @@
+import CurrencySelector from "./CurrencySelector"
+
 const ConverterForm = () => {
   return (
     <form className="converter-form">
@@ -9,15 +11,7 @@ const ConverterForm = () => {
       <div className="form-group form-currency-group">
         <div className="form-section">
             <label className="form-label">From</label>
-            <div className="currency-select">
-              <img src="https://flagsapi.com/US/flat/64.png" alt="Flag" />
-              <select className="currency-dropdown">
-                <option value="USD" selected>USD</option>
-                <option value="CAD">CAD</option>
-                <option value="INR">INR</option>
-                <option value="NPR">NPR</option>
-              </select>
-            </div>
+            <CurrencySelector/>
         </div>
 
       <div className="swap-icon">
@@ -36,15 +30,7 @@ const ConverterForm = () => {
 
         <div className="form-section">
             <label className="form-label">To</label>
-            <div className="currency-select">
-              <img src="https://flagsapi.com/IN/flat/64.png" alt="Flag" />
-              <select className="currency-dropdown">
-                <option value="USD">USD</option>
-                <option value="CAD">CAD</option>
-                <option value="INR" selected>INR</option>
-                <option value="NPR">NPR</option>
-              </select>
-            </div>
+            <CurrencySelector/>
         </div>
       </div>
       <button type="submit" className="submit-button">Get Exchange Rate</button>
